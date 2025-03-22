f7b0dffca7b6d49f01f5bf90915e89793cdde0f3
adi88d <adaud@paloaltonetworks.com>
2025-03-22 23:16:38 +0200

    create txt file response

--- slack_notifier.py
+++ slack_notifier.py
@@ -5,7 +5,7 @@ from collections.abc import Iterable
 def slack_notifier(slack_token, channel_id, removed_images ,added_images, failed_to_inspect_images):
 
     # removed_images = join_list_by_delimiter_in_chunks(removed_images, delimiter='\n* ')
-    added_images = join_list_by_delimiter_in_chunks(added_images, delimiter='\n* ')
+    # added_images = join_list_by_delimiter_in_chunks(added_images, delimiter='\n* ')
 
     # Initialize the WebClient with the token
     client = WebClient(token=slack_token)
@@ -22,31 +22,22 @@ def slack_notifier(slack_token, channel_id, removed_images ,added_images, failed
 
         # replay the old images removed message
         if removed_images:
-            with open('removed_images.txt', 'w') as f:
-                f.write('\n'.join(removed_images))
-            
-            upload_response = client.files_upload_v2(
-                channel='C04CHML16P8',
-                file='removed_images.txt',
-                title='Removed images',
+            client.chat_postMessage(
+                channel=channel_id,
                 text='*The following images removed:*',
                 thread_ts=message_ts  # Threaded message, using the timestamp of the original message
             )
-            # file_id = upload_response['file']['id']
             
-            # client.chat_postMessage(
-            #     channel=channel_id,
-            #     text='*The following images removed:*',
-            #     attachments=[
-            #         {
-            #             "fallback": "File not found",
-            #             "text": "Removed images",
-            #             "file_id": file_id  # Attach the uploaded file using its ID
-            #         }
-            #     ],
-            #     thread_ts=message_ts  # Threaded message, using the timestamp of the original message
-            # )
+            with open('removed_images.txt', 'w') as f:
+                f.write('\n'.join(removed_images))
             
+            client.files_upload_v2(
+                channel='C04CHML16P8',
+                file='removed_images.txt',
+                title='Removed images',
+                thread_ts=message_ts  # Threaded message, using the timestamp of the original message
+            )
+    
         else:
             client.chat_postMessage(
                 channel=channel_id,
@@ -54,27 +45,56 @@ def slack_notifier(slack_token, channel_id, removed_images ,added_images, failed
                 thread_ts=message_ts  # Threaded message, using the timestamp of the original message
             )
 
-
-
-        if added_images:
-            added_images_messages = ['*The following images added:*']
-            added_images_messages.extend(added_images)
-        else:
-            added_images_messages = ['*No new images has been added*']
-
         # replay the new images added message
-        for message in added_images_messages:
+        if added_images:
             client.chat_postMessage(
                 channel=channel_id,
-                text=message,
+                text='*The following images added:*',
                 thread_ts=message_ts  # Threaded message, using the timestamp of the original message
             )
+            
+            with open('added_images.txt', 'w') as f:
+                f.write('\n'.join(added_images))
+            
+            client.files_upload_v2(
+                channel='C04CHML16P8',
+                file='added_images.txt',
+                title='Added images',
+                thread_ts=message_ts  # Threaded message, using the timestamp of the original message
+            )
+    
+        else:
+            client.chat_postMessage(
+                channel=channel_id,
+                text='*No new images has been added*',
+                thread_ts=message_ts  # Threaded message, using the timestamp of the original message
+            )
+
+
+
+
+        # if added_images:
+        #     added_images_messages = ['*The following images added:*']
+        #     added_images_messages.extend(added_images)
+        # else:
+        #     added_images_messages = ['*No new images has been added*']
+
+        # # replay the new images added message
+        # for message in added_images_messages:
+        #     client.chat_postMessage(
+        #         channel=channel_id,
+        #         text=message,
+        #         thread_ts=message_ts  # Threaded message, using the timestamp of the original message
+        #     )
         
     except Exception as e:
         print(f"Error sending message: {str(e)}")
     finally:
         if os.path.exists('removed_images.txt'):
             os.remove('removed_images.txt')
+            
+        if os.path.exists('added_images.txt'):
+            os.remove('added_images.txt')
 
 
 def join_list_by_delimiter_in_chunks(list_to_join: Iterable[str], delimiter: str = ", ", max_length: int = 2_000) -> list[str]:
