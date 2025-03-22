99178276cfb3eb9cd9c4e08a4cd380d19d20c75e
adi88d <adaud@paloaltonetworks.com>
2025-03-22 23:05:19 +0200

    test slack

--- slack_notifier.py
+++ slack_notifier.py
@@ -26,7 +26,7 @@ def slack_notifier(slack_token, channel_id, removed_images ,added_images, failed
                 f.write('\n'.join(removed_images))
             
             upload_response = client.files_upload_v2(
-                channel=channel_id,
+                channel='C04CHML16P8',
                 file='removed_images.txt',
                 title='Removed images'
             )
